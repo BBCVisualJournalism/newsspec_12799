63ebd986aa612d4d5c603ecf7f1ea8e1584e9e80
Scott Jarvis <scott.jarvis@bbc.co.uk>
2016-04-28 17:13:36 +0100

    removed useless progressive enhancement stuff.. added some css for hiding map labels..

--- source/js/progressiveEnhancement.js
+++ source/js/progressiveEnhancement.js
@@ -5,11 +5,6 @@ define(['wrapper', 'jquery'], function (wrapper, $) {
     ProgressiveEnhancement.prototype = {
         init: function () {
             $('.bbc-news-vj-wrapper').addClass('bbc-news-vj-wrapper--js-enabled');
-            //to apply svg over png fallbacks
-            if (cutsTheMustard()){
-                $('.chart--one').addClass('chart--one--js-enabled');
-                $('.chart--two').addClass('chart--two--js-enabled');
-            }
         }
     };
 
