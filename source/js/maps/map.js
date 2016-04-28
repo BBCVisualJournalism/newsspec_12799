ae32be6f69da58614387b7b5021dde276ab7639b
Scott Jarvis <scott.jarvis@bbc.co.uk>
2016-04-28 16:03:28 +0100

    map labels now get values from vocabs.. images organised better..

--- source/js/maps/map.js
+++ source/js/maps/map.js
@@ -1,81 +1,121 @@
-define(["http://maps.google.com/maps/api/js?v=3.22", "maps/defaultStyles", "maps/textLabelTypes", "maps/markerTypes", "maps/bbcSettings", "maps/markers", "maps/BBCMap", "maps/BBCLabelOverlay", "maps/BBCCircleOverlay", "maps/geojson"],
-    function (GoogleMaps, defaultStyles, textLabelTypes, markerTypes, bbcSettings, markers, BBCMap, BBCLabelOverlay, BBCCircleOverlay, toll) {
+define(['http://maps.google.com/maps/api/js?v=3.22', 'maps/defaultStyles', 'maps/textLabelTypes', 'maps/markerTypes', 'maps/bbcSettings', 'maps/markers', 'maps/BBCMap', 'maps/BBCLabelOverlay', 'maps/BBCCircleOverlay', 'maps/geojson', 'vocabs'],
+    function (GoogleMaps, defaultStyles, textLabelTypes, markerTypes, bbcSettings, markers, BBCMap, BBCLabelOverlay, BBCCircleOverlay, toll, vocabs) {
         return function () {
 
-        var textLabels = [
+        var textLabels_map1 = [
             {
-                "lat": 39.5416447,
-                "lng": 21.7700136,
-                "text": "Greece",
-                "labelType": "country",
-                "direction": "rtl"
+                'lat': 39.5416447,
+                'lng': 21.7700136,
+                'text': vocabs.map1_label_5,
+                'labelType': 'country',
+                'direction': 'rtl'
             },{
-                "lat": 39.2917193,
-                "lng": 27.676603,
-                "text": "Turkey",
-                "labelType": "country"
+                'lat': 39.2917193,
+                'lng': 27.676603,
+                'text': vocabs.map1_label_6,
+                'labelType': 'country'
             },{
-                "lat": 39.3173691,
-                "lng": 26.6920916,
-                "text": "Ayvalik",
-                "labelType": "placeName"
+                'lat': 39.3173691,
+                'lng': 26.6920916,
+                'text': vocabs.map1_label_7,
+                'labelType': 'placeName'
+            }
+        ];
+
+        var textLabels_map2 = [
+            {
+                'lat': 39.5416447,
+                'lng': 21.7700136,
+                'text': vocabs.map2_label_5,
+                'labelType': 'country',
+                'direction': 'rtl'
+            },{
+                'lat': 39.2917193,
+                'lng': 27.676603,
+                'text': vocabs.map2_label_6,
+                'labelType': 'country'
+            },{
+                'lat': 39.3173691,
+                'lng': 26.6920916,
+                'text': vocabs.map2_label_7,
+                'labelType': 'placeName'
+            }
+        ];
+
+        var textLabels_map3 = [
+            {
+                'lat': 39.5416447,
+                'lng': 21.7700136,
+                'text': vocabs.map3_label_5,
+                'labelType': 'country',
+                'direction': 'rtl'
+            },{
+                'lat': 39.2917193,
+                'lng': 27.676603,
+                'text': vocabs.map3_label_6,
+                'labelType': 'country'
+            },{
+                'lat': 39.3173691,
+                'lng': 26.6920916,
+                'text': vocabs.map3_label_7,
+                'labelType': 'placeName'
             }
         ];
 
         var map1 = new BBCMap(
                 {
-                    "elementId": "newsspec_14121-map-canvas--one",
-                    "mapSettings": bbcSettings.one,
-                    "circleOverlays": [
+                    'elementId': 'newsspec_14121-map-canvas--one',
+                    'mapSettings': bbcSettings.one,
+                    'circleOverlays': [
                         {
-                            "dataset": toll.features,
-                            "valueProperty": "toll"
+                            'dataset': toll.features,
+                            'valueProperty': 'toll'
                         }
                     ],
-                    "textLabels": textLabels,
-                    "kmlLayers": [
-                        "https://dl.dropboxusercontent.com/u/7667577/maritime2.kml"
+                    'textLabels': textLabels_map1,
+                    'kmlLayers': [
+                        'https://dl.dropboxusercontent.com/u/7667577/maritime2.kml'
                     ],
-                    "markers": markers,
-                    "tileUrlBase": "../common/img/BBCTerrain/"
+                    'markers': markers,
+                    'tileUrlBase': '../common/img/BBCTerrain/'
                 }
             );
 
         var map2 = new BBCMap(
                 {
-                    "elementId": "newsspec_14121-map-canvas--two",
-                    "mapSettings": bbcSettings.two,
-                    "circleOverlays": [
+                    'elementId': 'newsspec_14121-map-canvas--two',
+                    'mapSettings': bbcSettings.two,
+                    'circleOverlays': [
                         {
-                            "dataset": toll.features,
-                            "valueProperty": "toll"
+                            'dataset': toll.features,
+                            'valueProperty': 'toll'
                         }
                     ],
-                    "textLabels": textLabels,
-                    "kmlLayers": [
-                        "https://dl.dropboxusercontent.com/u/7667577/maritime2.kml"
+                    'textLabels': textLabels_map2,
+                    'kmlLayers': [
+                        'https://dl.dropboxusercontent.com/u/7667577/maritime2.kml'
                     ],
-                    "markers": markers,
-                    "tileUrlBase": "../common/img/BBCTerrain/"
+                    'markers': markers,
+                    'tileUrlBase': '../common/img/BBCTerrain/'
                 }
             );
 
         var map3 = new BBCMap(
                 {
-                    "elementId": "newsspec_14121-map-canvas--three",
-                    "mapSettings": bbcSettings.three,
-                    "circleOverlays": [
+                    'elementId': 'newsspec_14121-map-canvas--three',
+                    'mapSettings': bbcSettings.three,
+                    'circleOverlays': [
                         {
-                            "dataset": toll.features,
-                            "valueProperty": "toll"
+                            'dataset': toll.features,
+                            'valueProperty': 'toll'
                         }
                     ],
-                    "textLabels": textLabels,
-                    "kmlLayers": [
-                        "https://dl.dropboxusercontent.com/u/7667577/maritime2.kml"
+                    'textLabels': textLabels_map3,
+                    'kmlLayers': [
+                        'https://dl.dropboxusercontent.com/u/7667577/maritime2.kml'
                     ],
-                    "markers": markers,
-                    "tileUrlBase": "../common/img/BBCTerrain/"
+                    'markers': markers,
+                    'tileUrlBase': '../common/img/BBCTerrain/'
                 }
             );
 
@@ -83,14 +123,14 @@ define(["http://maps.google.com/maps/api/js?v=3.22", "maps/defaultStyles", "maps
 
             function logSettings(el) {
                 for(var i = 0; i < Object.keys(bbcSettings).length; i++) {
-                    var p = document.createElement("p");
-                    p.innerText = Object.keys(bbcSettings)[i] + ": " + bbcSettings[Object.keys(bbcSettings)[i]];
+                    var p = document.createElement('p');
+                    p.innerText = Object.keys(bbcSettings)[i] + ': ' + bbcSettings[Object.keys(bbcSettings)[i]];
                     el.appendChild(p);
                 }
             }
 
             function initialize() {
-                //logSettings(document.getElementById("mapSettings"));
+                //logSettings(document.getElementById('mapSettings'));
                 map1.initialize();
                 map2.initialize();
                 map3.initialize();
--- source/js/maps/markerTypes.js
+++ source/js/maps/markerTypes.js
@@ -1,7 +1,7 @@
 define(['http://maps.google.com/maps/api/js?v=3.22'], function(GoogleMaps){
     markerTypes = {
         'grave': {
-            url: 'icons/gravestone.svg',
+            url: '../common/img/gravestone.svg',
             size: new google.maps.Size(16,17),
             origin: new google.maps.Point(0,0),
             anchor: new google.maps.Point(8, 8.5)
