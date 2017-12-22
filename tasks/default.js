4bf6f2b05de3357b5297082d5528cea4db733809
Olawale Malomo <warlay234@gmail.com>
2017-12-22 14:32:44 +0100

    Remove vj-wrapper:afterGrunt from Grunt default tasks

--- tasks/default.js
+++ tasks/default.js
@@ -1,3 +1,3 @@
 module.exports = function (grunt) {
-    grunt.registerTask('default', ['vj-wrapper:beforeGrunt', 'css', 'js', 'jsonlint', 'handlebars', 'create_vocab_modules', 'assets', 'vj-wrapper:afterGrunt']);
+    grunt.registerTask('default', ['vj-wrapper:beforeGrunt', 'css', 'js', 'jsonlint', 'handlebars', 'create_vocab_modules', 'assets']);
 };
